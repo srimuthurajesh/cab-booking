3774a5b801c718cca138b419801ad75e8d392d61
srimuthurajesh <srimuthurajesh@gmail.com>
2024-06-15 08:42:46 +0530

    latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,3 +77,42 @@ document.addEventListener('DOMContentLoaded', function () {
             });*/
     });
 });
+
+$(document).ready(function(){
+
+//DropDown input - select
+$('.t-dropdown-input').on('click', function() {
+    console.log("been caled");
+	$('.t-dropdown-list').slideDown('fast');
+});
+
+$('.t-select-btn').on('click', function() {
+   $('.t-dropdown-list').slideUp('fast');
+
+	if(!$(this).prev().attr('disabled')){
+    $(this).prev().trigger('click');
+	}
+});
+
+//$('.t-dropdown-input').width($('.t-dropdown-select').width() - $('.t-select-btn').width() - 13);
+
+//width for t-list
+$('.t-dropdown-list').width($('.t-dropdown-input').width());
+
+$('.t-dropdown-input').val('');
+
+$('li.t-dropdown-item').on('click', function() {
+  var text = $(this).html();
+  $('.t-dropdown-input').val(text);
+  $('.t-dropdown-list').slideUp('fast');
+});
+
+$(document).on('click', function(event) {
+  if ($(event.target).closest(".t-dropdown-input, .t-select-btn").length)
+    return;
+  $('.t-dropdown-list').slideUp('fast');
+  event.stopPropagation();
+});
+// END //
+
+});
